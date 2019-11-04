324788886dad2e76b761d279fa0914d628ff73a6
Igor <igor@devfire.io>
2019-11-03 23:03:18 -0500

    added documentation to working code

--- processor.py
+++ processor.py
@@ -38,12 +38,12 @@ def validate_json_schema(payload):
     '''
     In a JSON Schema, by default properties are not required, all that our schema does 
     is state what type they must be if the property is present. 
-    So for validation to flag whatever additional properties are missing, 
+    So, for validation to flag whatever additional properties are missing, 
     we need to mark that key as a required property first, 
     by adding a required list with names. For more info:
     https://json-schema.org/understanding-json-schema/reference/object.html#required-properties
 
-    Note the "required" property at the end of the schema.
+    NOTE: the "required" property is at the end of the schema.
     '''
     schema = {
         "type" : "object",
@@ -85,6 +85,7 @@ def send_value_to_blynk(dest_mac_address, dest_feedname, dest_value):
         URL = BLYNK_URL + BLYNK_AUTH + '/update/V' + str(pin) + '?value=' + str(value)
         logging.info("Sending " + str(value) + " to pin " + str(pin))
 
+        # attempt to send data to blynk
         try:    
             response = requests.get(URL)
         except requests.exceptions.RequestException as e:
@@ -93,25 +94,17 @@ def send_value_to_blynk(dest_mac_address, dest_feedname, dest_value):
         # either mac or the feedname is not found, skipping
         logging.error("Not sure what to do with " + str(dest_feedname) + " from " + str(dest_mac_address))
 
-
-''' We need to make sure the env vars are set correctly.
-Assuming loglevel is bound to the string value obtained from the
-command line argument. Convert to upper case to allow the user to
-specify --log=DEBUG or --log=debug
-numeric_level = getattr(logging, loglevel.upper(), None)
-if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % loglevel)
-logging.basicConfig(level=numeric_level)
-'''
-
+# set our default log level
 logging.basicConfig(level=logging.INFO)
 
+# initialize the config parser
 config = configparser.ConfigParser()                                     
 config.read('./settings.ini')
 
 # setup the server once
 udp_server_socket = init_udp_server()
 
+# both the blynk url and the auth token must be present
 for env_var in ('BLYNK_URL','BLYNK_AUTH'):
     logging.info("Checking for " + str(env_var))
     if env_var in os.environ:
@@ -124,16 +117,29 @@ for env_var in ('BLYNK_URL','BLYNK_AUTH'):
 BLYNK_AUTH = os.getenv("BLYNK_AUTH")
 BLYNK_URL = os.getenv("BLYNK_URL")
 
+'''
+Let's get the infinite loop going.
+Essentially, udp_server_socket.recvfrom(1024) will block, waiting for a new message.
+Once the message is received, we need to:
+1. make sure it's actually JSON
+2. make sure all the required fields are present
+3. publish to blynk
+4. (optionally) do what else needs to be done
+'''
 while(True):
     bytesAddressPair = udp_server_socket.recvfrom(1024)
 
+    # this is the actual message
     message = bytesAddressPair[0]
+
+    # source IP addressed, ignored for now
     address = bytesAddressPair[1]
 
     '''
     First, make sure the udp payload is a valid json string.
     If it is, valid_json is set to True and message contains the JSON object.
-    If not, valid_json is set to False and message is ''
+    We do it in one shot since we are much more likely to get valid JSON than not.
+    If not, valid_json is set to False and message is an empty string ''.
     '''
     valid_json_bool, client_message = is_valid_json(message)
 
@@ -152,9 +158,18 @@ while(True):
     # print the received message for debugging purposes
     logging.debug(client_message)
 
-    #sensor_data = json.loads(client_message)
+    '''
+    Let's get all the things assigned to the right values:
+    - value is the actual numeric value of the sensor
+    - mac is the mac address, this is how we figure out what to route where
+    - feed name is used to look up the blynk virtual pin
+    '''
     value = client_message["value"]
     mac   = client_message["mac"]
     feed_name = client_message["feedName"]
     
+    '''
+    Publish the data to blynk.
+    NOTE: we don't know the correct pin, we'll look it up later
+    '''
     send_value_to_blynk(mac, feed_name, value)
