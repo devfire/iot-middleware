1892cc3a2e36d9343e8b53c6b1e75cc315d0141e
Igor <igor@devfire.io>
2019-11-12 17:37:16 -0500

    added settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+import configparser
+import logging
+
+# set our default log level
+LOG_LEVEL = logging.INFO
+
+# set the default config file
+CONFIG_FILE = 'settings.ini'
+
+# initialize the config parser
+config = configparser.ConfigParser()
+
+
