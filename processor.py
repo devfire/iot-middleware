cb181a6f268450a6e1ca141940add22f191125f9
Igor <igor@devfire.io>
2019-12-11 13:15:04 -0500

    renamed settings to shared

--- processor.py
+++ processor.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 import requests
-import settings
+import shared
 import os
 import validators
 
@@ -15,24 +15,24 @@ def init_udp_server():
     # Datagram (udp) socket
     try:
         UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        settings.logger.debug("Socket created")
+        shared.logger.debug("Socket created")
     except OSError as err:
-        settings.logger.error(err)
+        shared.logger.error(err)
         sys.exit()
 
     # Bind socket to host and port
     try:
         UDPServerSocket.bind((HOST, PORT))
     except OverflowError as err:
-        settings.logger.error(err)	
+        shared.logger.error(err)	
         sys.exit()
 
-    settings.logger.info("UDP server is ready to go.")
+    shared.logger.info("UDP server is ready to go.")
 
     return UDPServerSocket
 
 def send_value_to_blynk(client_message):
-    # blynk settings
+    # blynk shared
     BLYNK_AUTH = os.getenv("BLYNK_AUTH")
     BLYNK_URL = os.getenv("BLYNK_URL")
 
@@ -46,26 +46,26 @@ def send_value_to_blynk(client_message):
     mac   = client_message["mac"]
     feed_name = client_message["feedName"]
 
-    settings.logger.debug("Checking the config file for the pin settings")
-    # check to see if there's a relevant section in settings.ini and that section has the pin
-    if settings.config.has_option(mac,feed_name):
+    shared.logger.debug("Checking the config file for the pin shared")
+    # check to see if there's a relevant section in shared.ini and that section has the pin
+    if shared.config.has_option(mac,feed_name):
         # it does, let's grab its pin
-        pin = settings.config[mac][feed_name]
-        settings.logger.debug("Found valid config for pin " + str(pin))
+        pin = shared.config[mac][feed_name]
+        shared.logger.debug("Found valid config for pin " + str(pin))
 
         # format the URL properly. This is a REST call to blynk.
         URL = BLYNK_URL + '/' + BLYNK_AUTH + '/update/V' + str(pin) + '?value=' + str(value)
-        settings.logger.debug("Posting to " + URL)
+        shared.logger.debug("Posting to " + URL)
 
         # attempt to send data to blynk
         try:    
             response = requests.get(URL)
-            settings.logger.debug("Sent data successfully to " + URL)
+            shared.logger.debug("Sent data successfully to " + URL)
         except requests.exceptions.RequestException as err:
-            settings.logger.error(err)
+            shared.logger.error(err)
     else:
         # either mac or the feedname is not found, skipping
-        settings.logger.error("Not sure what to do with " + str(feed_name) + " from " + str(mac))
+        shared.logger.error("Not sure what to do with " + str(feed_name) + " from " + str(mac))
 
 def main(udp_server_socket):
     '''
@@ -77,7 +77,7 @@ def main(udp_server_socket):
     3. publish to blynk
     4. (optionally) do what else needs to be done
     '''
-    settings.logger.debug("Starting the processor main loop.")
+    shared.logger.debug("Starting the processor main loop.")
     while(True):
         bytesAddressPair = udp_server_socket.recvfrom(1024)
 
@@ -87,7 +87,7 @@ def main(udp_server_socket):
         # source IP addressed, ignored for now
         address = bytesAddressPair[1]
 
-        settings.logger.debug("Received " + str(message) + "from " + str(address))
+        shared.logger.debug("Received " + str(message) + "from " + str(address))
 
         '''
         First, make sure the udp payload is a valid json string.
@@ -101,25 +101,32 @@ def main(udp_server_socket):
         if (valid_json_bool):
             # OK, so it is JSON. Let's make sure it is semantically valid
             if (validators.validate_json_schema(current_client_message)):
-                settings.logger.debug("Valid schema detected!")
+                shared.logger.debug("Valid schema detected!")
             else:
-                settings.logger.error("Failed schema validation, skipping.")
+                shared.logger.error("Failed schema validation, skipping.")
                 continue # go to the beginning of the while loop
         else:
-            settings.logger.error("Could not serialize payload to JSON, skipping.")
+            shared.logger.error("Could not serialize payload to JSON, skipping.")
             continue # go to the beginning of the while loop
         
         # print the received message for debugging purposes
-        settings.logger.debug(current_client_message)
+        shared.logger.debug(current_client_message)
         
         '''
         Publish the data to blynk.
-        NOTE: we don't know the correct pin, we'll look it up later
+        NOTE: we don't know the correct virtual pin, we'll look it up later
         '''
         send_value_to_blynk(current_client_message)
 
 if __name__ == '__main__':
+    # make sure all of the required env vars are present
     validators.validate_env_variables()
+
+    # make sure the config file is present
     validators.validate_config_file()
+
+    # initialize the server, get the socket back
     server_socket = init_udp_server()
+
+    # start the infinite loop, pass the socket as a parameter
     main(server_socket)
--- validators.py
+++ validators.py
@@ -1,48 +1,48 @@
 import os
-import settings
+import shared
 import sys
 import json
 
 def validate_env_variables():
     # both the blynk url and the auth token must be present
     for env_var in ('BLYNK_URL','BLYNK_AUTH'):
-        settings.logger.info("Checking for " + str(env_var))
+        shared.logger.debug("Checking for " + str(env_var))
         if env_var in os.environ:
-            settings.logger.info("Found " + str(env_var))
+            shared.logger.debug("Found " + str(env_var))
         else:
-            settings.logger.error("Missing required environment variable: " + str(env_var))
+            shared.logger.error("Missing required environment variable: " + str(env_var))
             sys.exit(1)
 
 def validate_config_file():
     # make sure the config file actually exists
     try:
-        settings.config.read_file(open(settings.CONFIG_FILE))
+        shared.config.read_file(open(shared.CONFIG_FILE))
     except FileNotFoundError:
-        settings.logger.error("Missing config file, exiting.")
+        shared.logger.error("Missing config file, exiting.")
         sys.exit(1)
 
 def validate_json_schema(payload):
     valid_json_schema = None
-    settings.logger.debug("Validating " + str(payload))
+    shared.logger.debug("Validating " + str(payload))
     try:
-        settings.jsonschema.validate(payload, settings.schema)
+        shared.jsonschema.validate(payload, shared.schema)
         valid_json_schema = True
-    except settings.jsonschema.exceptions.ValidationError as ve:
-        settings.logger.error(ve)
+    except shared.jsonschema.exceptions.ValidationError as ve:
+        shared.logger.error(ve)
         valid_json_schema = False
     return valid_json_schema
 
 def validate_json(udp_payload):
     payload_is_json = None
-    settings.logger.debug("Validating " + str(udp_payload))
+    shared.logger.debug("Validating " + str(udp_payload))
     # make sure we were actually passed a JSON object
     try:
         json_payload = json.loads(udp_payload)
-        settings.logger.debug("Valid JSON detected " + str(udp_payload))
+        shared.logger.debug("Valid JSON detected " + str(udp_payload))
         payload_is_json = True
     except:
         json_payload = ''
-        settings.logger.error("Invalid JSON detected " + str(udp_payload))
+        shared.logger.error("Invalid JSON detected " + str(udp_payload))
         payload_is_json = False
     
     # return a tuple of boolean and JSON payload
